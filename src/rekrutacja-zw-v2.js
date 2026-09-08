e1f917232323b513f59b3b40a43cb525cd61742f
kaczorkacper28 <kaczorkacper28@gmail.com>
2026-09-08 23:33:20 +0200

    Usuń pusty nieużywany moduł rekrutacji

--- src/rekrutacja-zw-v2.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-  stages:['PUBLICZNE','KANDYDATA','EGZAMIN_KONCOWY'],
-  description:'Trzystopniowa rekrutacja RP Żandarmerii Wojskowej: podanie publiczne, rekrutacja kandydata, egzamin końcowy.'
-};
