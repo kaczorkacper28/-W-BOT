667826f8298ff0fa7eccda75b7754baeb6be6f12
kaczorkacper28 <kaczorkacper28@gmail.com>
2026-09-08 23:28:20 +0200

    Dodaj definicję trzystopniowej rekrutacji ŻW

---
+++ src/rekrutacja-zw-v2.js
@@ -0,0 +1,4 @@
+module.exports={
+  stages:['PUBLICZNE','KANDYDATA','EGZAMIN_KONCOWY'],
+  description:'Trzystopniowa rekrutacja RP Żandarmerii Wojskowej: podanie publiczne, rekrutacja kandydata, egzamin końcowy.'
+};
